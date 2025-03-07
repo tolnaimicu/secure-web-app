b1b3bf7e70a348ea7325ec1e7a879b71fe244b20
Tolnai <tolnai.milan22@gmail.com>
2025-03-07 14:35:54 +0100

    Initial commit

---
+++ Models/ItemModel.cs
@@ -0,0 +1,12 @@
+namespace RankingApp.Models
+{
+    public class ItemModel
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public int ImageId { get; set; }
+        public int Ranking { get; set; }
+        public int ItemType { get; set; }
+
+    }
+}
